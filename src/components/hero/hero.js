569b50b6455db0715d30ebc0f6a4f2b37f086e68
Simon Weller <s.meierbeck@gmail.com>
2018-09-14 21:35:11 +0200

    Added propTypes for hero component

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Img from 'gatsby-image';
 import './hero.module.css';
 
@@ -19,3 +20,15 @@ const Hero = ({ image }) => (
 );
 
 export default Hero;
+
+Hero.propTypes = {
+  image: PropTypes.shape({
+    sizes: PropTypes.shape({
+      aspectRatio: PropTypes.number.isRequired,
+      base64: PropTypes.string.isRequired,
+      sizes: PropTypes.string.isRequired,
+      src: PropTypes.string.isRequired,
+      srcSet: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
